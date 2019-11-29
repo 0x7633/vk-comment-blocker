d9ab196ce4c237bff8e423036b4a004083df1313
0x7633 <0x7633@gmail.com>
2019-11-29 15:43:14 +0800

    Adapt to a new design

--- extension/scripts/inject.js
+++ extension/scripts/inject.js
@@ -2,41 +2,30 @@
 if (!document.body.classList.contains('vkcb')) {
     document.body.classList.add('vkcb');
 }
-function addButtons() {
+
+function changeButtons() {
     var postRepliesEls = document.querySelectorAll('div.post_info');
     [].forEach.call(postRepliesEls, function (node) {
-        var postFullLikeEls = node.querySelector('div.post_full_like_wrap');
-        if (!postFullLikeEls.querySelector('.vkcb-btn')) {
-            var postLikeButton = postFullLikeEls.querySelector('.post_like');
-            var postId = postLikeButton.getAttribute('onmouseover').match(/\d+_\d+/)[0];
-            var buttonEl = document.createElement('a');
-            buttonEl.href = '#';
-            buttonEl.classList.add('post_like', '_like_wrap', 'vkcb-btn');
-            buttonEl.setAttribute('onclick', "this.closest('.post_info').querySelector('.replies_wrap').classList.toggle('vkcb-show'); return false;");
-            var buttonIconEl = document.createElement('i');
-            buttonIconEl.classList.add('icon');
-            var commentsEl = node.querySelector('.wr_header');
-            var commentsCount = 0;
-            if (commentsEl) {
-                commentsCount = parseInt(commentsEl.getAttribute('offs').replace(/[^\/]+\//, ''), 10);
+        if (!node.querySelector('.vkcb-btn')) {
+            var repliesWrap = "this.closest('.post_info').querySelector('.replies_wrap').classList.toggle('vkcb-show'); ";
+            var repliesHeader = "this.closest('.post_info').querySelector('.post_replies_header').classList.toggle('show-block'); ";
+            if (node.querySelector('.like_btn.comment') && node.querySelector('.post_replies_header')) {
+                node.querySelector('.like_btn.comment').classList.add('vkcb-btn');
+                node.querySelector('.like_btn.comment').setAttribute('onclick', repliesWrap + repliesHeader + "return false;");
             }
-            else {
-                commentsCount = node.getElementsByClassName('reply').length;
+            else if (node.querySelector('.like_btn.comment') && !node.querySelector('.post_replies_header')) {
+                node.querySelector('.like_btn.comment').classList.add('vkcb-btn');
+                node.querySelector('.like_btn.comment').setAttribute('onclick', repliesWrap + "return false;");
             }
-            var buttonCountEl = document.createElement('span');
-            buttonCountEl.classList.add('post_share_count', '_count');
-            buttonCountEl.textContent = commentsCount.toString();
-            buttonEl.appendChild(buttonIconEl);
-            buttonEl.appendChild(buttonCountEl);
-            postFullLikeEls.appendChild(buttonEl);
         }
     });
 }
+
 chrome.runtime.sendMessage({ method: 'getLocalStorage' }, function (res) {
     if (!res.globalStatus) {
-        addButtons();
+        changeButtons();
         var observer = new MutationObserver(function () {
-            addButtons();
+            changeButtons();
         });
         observer.observe(document.getElementById('content'), {
             childList: true,
